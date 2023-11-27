ac85e42703b4e1fd8390e1d7059e00b8301da8f9
Robin Bergewski <robinbergewski@gmail.com>
2023-11-27 21:50:21 +0100

    Change: build.zig

--- build.zig
+++ build.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "zargs",
         .root_source_file = .{ .path = "main.zig" },
+        .target = target,
+        .optimize = optimize,
     });
 
     b.installArtifact(exe);
@@ -12,6 +17,9 @@ pub fn build(b: *std.Build) void {
     exe.addModule("clap", zap.module("clap"));
 
     const run_exe = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
